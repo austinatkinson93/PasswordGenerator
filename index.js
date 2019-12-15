e38ef0089fd752a5bb1caf223b01e5beb49ea8c1
austinatkinson93 <austinatkinson93@users.noreply.github.com>
2019-12-15 15:32:18 -0500

    added function prompts to button click

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-var capLettersArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+
+// create variable arrays
+var uppLettersArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 var lowLettersArray = "abcdefghijklmnopqrstuvwxyz".split("")
 var specialArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ '".split("")
 var numbersArray = "1234567890".split("")
 
-var pasLength = prompt("how long would you like your password to be?")
-var capLettersAnswer = prompt("would you like your password to contain capital letters?")
-var lowLettersAnswer = prompt("would you like your password to contain lowercase letters?")
-var specialAnswer = prompt("would you like your password to contain special characters?")
-var numbersAnswer = prompt("would you like your password to contain numbers?")
 
 
-//generate random password
-function generate(){
+//generate random functions
+function getRandomUpper() {
+    return uppLettersArray[Math.floor(Math.random() * 26)]
+}
+function getRandomLower() {
+    return lowLettersArray[Math.floor(Math.random() * 26)]
+}
+function getRandomSpecial() {
+    return specialArray[Math.floor(Math.random() * 31)]
+}
+function getRandomNumber() {
+    return numbersArray[Math.floor(Math.random() * 10)]
+}
 
+
+function generatePassword() {
+    
+    // create userinput prompts
+    var pasLength = prompt("how long would you like your password to be?")
+    var capLettersAnswer = confirm("would you like your password to contain capital letters?")
+    var lowLettersAnswer = confirm("would you like your password to contain lowercase letters?")
+    var specialAnswer = confirm("would you like your password to contain special characters?")
+    var numbersAnswer = confirm("would you like your password to contain numbers?")
+
+    for (var i = 0; i < pasLength.length; i++) {
+
+        if (capLettersAnswer === true && lowLettersAnswer === true && specialAnswer === true && numbersAnswer === true) {
+            var allChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~1234567890".split("")
+            return 
+
+        }
+    }
 }
