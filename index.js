71dd75333339d54b2bf429cccb7f0443296d4727
austinatkinson93 <austinatkinson93@users.noreply.github.com>
2019-12-15 13:59:08 -0500

    added variable arrays and prompts.

---
+++ index.js
@@ -0,0 +1,16 @@
+var capLettersArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+var lowLettersArray = "abcdefghijklmnopqrstuvwxyz".split("")
+var specialArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ '".split("")
+var numbersArray = "1234567890".split("")
+
+var pasLength = prompt("how long would you like your password to be?")
+var capLettersAnswer = prompt("would you like your password to contain capital letters?")
+var lowLettersAnswer = prompt("would you like your password to contain lowercase letters?")
+var specialAnswer = prompt("would you like your password to contain special characters?")
+var numbersAnswer = prompt("would you like your password to contain numbers?")
+
+
+//generate random password
+function generate(){
+
+}
